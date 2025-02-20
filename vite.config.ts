a4aa80bc48d9534a5f7154f2c414e084dcbff7a2
Dash Skndash <dashskndash@gmail.com>
2025-02-20 12:56:30 +0530

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,29 +5,27 @@ import { VitePWA } from 'vite-plugin-pwa'
 import dotenv from 'dotenv'
 dotenv.config()
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({
-    manifest: {
-      name: 'Ride NITT | Carpool like never before',
-      short_name: 'Ride NITT',
-      description: 'Ride NITT is a carpooling platform for the students of NIT Trich by the Technical Club of NIT Trichy',
-      theme_color: '#008955',
-      icons: [
-        {
-          src: 'logo192.png',
-          sizes: '192x192',
-          type: 'image/png'
-        },
-        {
-          src: 'logo512.png',
-          sizes: '512x512',
-          type: 'image/png'
-        }
-      ]
-    },
-    includeAssets: ['favicon.ico'],
-  })],
+  // plugins: [react(), VitePWA({
+  //   manifest: {
+  //     name: 'Ride NITT | Carpool like never before',
+  //     short_name: 'Ride NITT',
+  //     description: 'Ride NITT is a carpooling platform for the students of NIT Trich by the Technical Club of NIT Trichy',
+  //     theme_color: '#008955',
+  //     icons: [
+  //       {
+  //         src: 'logo192.png',
+  //         sizes: '192x192',
+  //         type: 'image/png'
+  //       },
+  //       {
+  //         src: 'logo512.png',
+  //         sizes: '512x512',
+  //         type: 'image/png'
+  //       }
+  //     ]
+  //   }
+  // })],
   server: {
     proxy: {
       '/api': {
