afce0f16f0273ea53db0e8f804217d4a17744f25
Dash Skndash <dashskndash@gmail.com>
2025-02-21 02:23:17 +0530

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,9 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 
 import dotenv from 'dotenv'
 dotenv.config()
 
 export default defineConfig({
-  plugins: [react(), VitePWA({
-    manifest: {
-      name: 'Ride NITT | Carpool like never before',
-      short_name: 'Ride NITT',
-      description: 'Ride NITT is a carpooling platform for the students of NIT Trich by the Technical Club of NIT Trichy',
-      theme_color: '#008955',
-      icons: [
-        {
-          src: 'logo192.png',
-          sizes: '192x192',
-          type: 'image/png'
-        },
-        {
-          src: 'logo512.png',
-          sizes: '512x512',
-          type: 'image/png'
-        }
-      ]
-    }
-  })],
   server: {
     //vite proxy is not needed in production, vercel rewrite rules are used instead
     proxy: {
