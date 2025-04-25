b4eaeb75cf53fb9e9f1ab4172b68b1c67df5398b
Shikhar Mohan Gautam <shikharmgautam@gmail.com>
2025-04-25 05:20:46 +0000

    correct date shift

--- src/Components/DateTimeModal.tsx
+++ src/Components/DateTimeModal.tsx
@@ -73,7 +73,7 @@ export function DateTimeModal({
 
   const handleConfirm = () => {
     onConfirm({
-      date: selectedDate+1,
+      date: selectedDate,
       month: currentMonth,
       year: currentYear,
       startHour: Number(startHour),
